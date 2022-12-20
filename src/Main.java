4a9f4cd3d4e1f5bec4f66214683055fa50ac045a
Goncharov_Mikhail <mishgun-nzk@mail.ru>
2022-12-20 20:02:39 +0700

    ДЗ добавлено

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+class Homework { // класс Homework
+    public static void main(String[] args) { // Метод main
+        task1(); // Объявили метод «задача1»
+        task2(); // Объявили метод «задача2»
+    }
+
+    public static void task1() { // Вызвали метод «задача1»
+        // Пишем код первого задания
+    }
+
+    public static void task2() { // Вызвали метод «задача2»
+        // Пишем код второго задания
+    }
+}
